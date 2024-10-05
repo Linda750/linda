e57e08d5692dfa70824db76e1527d093fca1b1fb
Linda750 <2300708138@mubs.ac.ug>
2024-10-05 03:18:25 -0700

    Add files via upload

---
+++ src/accounts/Acountlogin.java
@@ -0,0 +1,36 @@
+package accounts;
+
+import java.util.Scanner;
+
+public class Acountlogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String username = "linda";
+		String password = "Lin256";
+	
+		System.out.println("Enter username");
+		
+		System.out.println("Enter password");
+			
+		System.out.println("login successful");
+		System.out.println("invalid login");
+		
+		for(int y = 1;y<=3;y++){
+			System.out.println("y");
+			if(y==2){
+				break;
+			}
+			
+			
+	
+		  
+	}
+ 
+
+	}
+
+}
